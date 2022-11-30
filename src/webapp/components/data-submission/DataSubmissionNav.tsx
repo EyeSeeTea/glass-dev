1d3ecbd76b4d45e4e86df6790ed919d9a717b0a9
xell12686 <jlacandula@gmail.com>
2022-11-30 19:38:45 +0800

    wip: dnyamic content for each step

--- src/webapp/components/data-submission/DataSubmissionNav.tsx
+++ src/webapp/components/data-submission/DataSubmissionNav.tsx
@@ -6,6 +6,7 @@ import i18n from "../../../locales";
 import { useDataSubmissionSteps } from "../../hooks/userDataSubmissionSteps";
 import { useAppContext } from "../../contexts/app-context";
 import { CircularProgress } from "material-ui";
+import { glassColors } from "../../pages/app/themes/dhis2.theme";
 export interface DataSubmissionStep {
     stepNumber: number;
     title: string;
@@ -18,16 +19,21 @@ export interface DataSubmissionWizard {
 
 interface DataSubmissionNavProps {
     steps?: DataSubmissionStep[];
+    currentStep: number;
+    changeStep: (step: number) => void;
 }
 
-export const DataSubmissionNav: React.FC<DataSubmissionNavProps> = ({ steps }) => {
+export const DataSubmissionNav: React.FC<DataSubmissionNavProps> = props => {
+    const { steps, currentStep, changeStep } = props;
+
     return (
         <NavContainer>
             {steps?.length && (
                 <ul>
                     {steps.map(step => (
-                        <li key={step.stepNumber}>
-                            <Button>{step.title}</Button>
+                        <li key={step.stepNumber} className={currentStep === step.stepNumber ? "current" : ""}>
+                            <div className="number">{step.stepNumber}</div>
+                            <Button onClick={() => changeStep(step.stepNumber)}>{step.title}</Button>
                         </li>
                     ))}
                 </ul>
@@ -39,9 +45,35 @@ export const DataSubmissionNav: React.FC<DataSubmissionNavProps> = ({ steps }) =
 const NavContainer = styled.div`
     ul {
         display: flex;
+        margin: 0;
         flex-direction: row;
         gap: 20px;
         align-items: center;
         justify-content: center;
+        list-style-type: none;
+    }
+    li {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        opacity: 0.5;
+        &.current {
+            opacity: 1;
+        }
+    }
+    .number {
+        background-color: ${glassColors.mainPrimary};
+        color: white;
+        border-radius: 100%;
+        height: 20px;
+        width: 20px;
+        line-height: 20px;
+        text-align: center;
+        font-size: 12px;
+        flex: none;
+    }
+    button {
+        line-height: 1.4;
+        text-align: left;
     }
 `;
