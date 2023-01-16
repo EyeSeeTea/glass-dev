080c2b0c2e8dd26b878b7c4991eacb83027bec8f
Sneha N <9sneha.n@gmail.com>
2023-01-16 23:44:42 +0530

    UI for submission status of call

--- src/webapp/components/current-call/overview/CurrentStatus.tsx
+++ src/webapp/components/current-call/overview/CurrentStatus.tsx
@@ -2,50 +2,45 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import styled from "styled-components";
 import i18n from "@eyeseetea/d2-ui-components/locales";
-import { glassColors } from "../../../pages/app/themes/dhis2.theme";
+import { CtaButtons } from "./CtaButtons";
+import { StatusCTAs } from "./StatusTypes";
 
-export const CurrentStatus: React.FC = () => {
+interface StatusProps {
+    moduleName: string;
+    title: string;
+    description: string;
+    statusColor: string;
+    ctas: StatusCTAs[];
+}
+
+export const CurrentStatus: React.FC<StatusProps> = ({ moduleName, title, description, statusColor, ctas }) => {
     return (
-        <ContentWrapper>
+        <div>
             <Box sx={{ m: 2 }} />
-            <div className="status-box">
-                <small>{i18n.t("Current Status")}</small>
-                <span className="status not-completed">{i18n.t("Not Completed")}</span>
+            <div>
+                <StyledCurrentStatusStr>{i18n.t("Current Status")}</StyledCurrentStatusStr>
+                <StyledStatus statusColor={statusColor}>{i18n.t(title)}</StyledStatus>
             </div>
             <Box sx={{ m: 2 }} />
-            <p>{i18n.t("At least one dataset is missing")}</p>
-            <p>{i18n.t("At least one mandatory questionnaire is not submited")}</p>
-            <p>
-                <strong>
-                    {i18n.t("You need to complete the mandatory uploads before validate the submissions for this call")}
-                </strong>
-            </p>
-        </ContentWrapper>
+            <StyledDescription>{i18n.t(description)}</StyledDescription>
+            <CtaButtons moduleName={moduleName} ctas={ctas} />
+        </div>
     );
 };
 
-const ContentWrapper = styled.div`
-    h3,
-    h4 {
-        margin: 0;
-    }
-    p {
-        margin: 0;
-        line-height: 1.4;
-    }
-    .status-box {
-        text-transform: uppercase;
-        small {
-            font-weight: bold;
-            font-size: 13px;
-            display: block;
-            opacity: 0.7;
-        }
-        .status {
-            font-weight: 500;
-            &.not-completed {
-                color: ${glassColors.orange};
-            }
-        }
-    }
+const StyledCurrentStatusStr = styled.small`
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 13px;
+    display: block;
+    opacity: 0.7;
+`;
+const StyledStatus = styled.span<{ statusColor: string }>`
+    text-transform: uppercase;
+    font-weight: 500;
+    color: ${props => props.statusColor};
+`;
+const StyledDescription = styled.p`
+    margin: 0;
+    line-height: 1.4;
 `;
