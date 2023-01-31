d29b2cc794d7d455455a62dd9726bf74fa0376dd
James Lacandula <jlacandula@gmail.com>
2023-01-31 19:56:16 +0800

    glass module provider

--- src/webapp/contexts/glass-module-context.ts
+++ src/webapp/contexts/glass-module-context.ts
@@ -1,17 +1,13 @@
 import { createContext, useContext } from "react";
 
 export interface UrlModuleParams {
-    module: string, orgUnit: string
+    module: string;
+    orgUnit: string;
 }
 
+export const GlassModuleContext = createContext<UrlModuleParams>({
+    module: "",
+    orgUnit: "",
+});
 
-export const GlassModuleContext = createContext<string | null>(null);
-
-export function useGlassModuleContext() {
-    const context = useContext(GlassModuleContext);
-    if (context) {
-        return context;
-    } else {
-        throw new Error("Glass Module context uninitialized");
-    }
-}
+export const useGlassModuleContext = () => useContext(GlassModuleContext);
