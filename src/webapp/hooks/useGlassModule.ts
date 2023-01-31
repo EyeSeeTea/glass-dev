deb62c652428d0b8a891d643c897edd120ff575a
James Lacandula <jlacandula@gmail.com>
2023-02-01 01:22:27 +0800

    apply glassmodule provider to current-call page

--- src/webapp/hooks/useGlassModule.ts
+++ src/webapp/hooks/useGlassModule.ts
@@ -1,21 +1,27 @@
 import React from "react";
 import { CompositionRoot } from "../../CompositionRoot";
 import { GlassModule } from "../../domain/entities/GlassModule";
+import { useGlassModuleContext } from "../contexts/glass-module-context";
 import { GlassState } from "./State";
 
 export type GlassModuleState = GlassState<GlassModule>;
 
-export function useGlassModule(compositionRoot: CompositionRoot, name: string) {
+export function useGlassModule(compositionRoot: CompositionRoot) {
+    
+    const { module: moduleName } = useGlassModuleContext();
+
     const [result, setResult] = React.useState<GlassModuleState>({
         kind: "loading",
     });
 
     React.useEffect(() => {
-        compositionRoot.glassModules.getByName(name).run(
-            module => setResult({ kind: "loaded", data: module }),
-            error => setResult({ kind: "error", message: error })
-        );
-    }, [compositionRoot, name]);
+        if (moduleName) {
+            compositionRoot.glassModules.getByName(moduleName).run(
+                module => setResult({ kind: "loaded", data: module }),
+                error => setResult({ kind: "error", message: error })
+            );
+         }
+    }, [compositionRoot, moduleName]);
 
     return result;
 }
