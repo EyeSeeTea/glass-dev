a893061b61e90b4a4696cecfa738084f385c0762
James Lacandula <jlacandula@gmail.com>
2023-01-31 19:56:16 +0800

    glass module provider

---
+++ src/webapp/contexts/GlassModuleProvider.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GlassModuleContext } from "./glass-module-context";
+
+export const GlassModuleContextProvider: React.FC = ({ children }) => {
+    const { module = "", orgUnit = "" } = useParams<{
+      module?: string;
+      orgUnit?: string;
+    }>();
+  
+    const [glassModule, setGlassModule] = useState({ module, orgUnit });
+  
+    useEffect(() => {
+      setGlassModule({ module, orgUnit });
+    }, [module, orgUnit]);
+  
+    return (
+      <GlassModuleContext.Provider value={glassModule}>
+        {children}
+      </GlassModuleContext.Provider>
+    );
+  };
