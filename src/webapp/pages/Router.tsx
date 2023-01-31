fff959ab3090eab5bf4b93a38c25f938ea1aa8ac
James Lacandula <jlacandula@gmail.com>
2023-01-31 20:17:31 +0800

    setup Current glass module provider to current call page

--- src/webapp/pages/Router.tsx
+++ src/webapp/pages/Router.tsx
@@ -1,47 +1,39 @@
 import React from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
+import { GlassModuleContextProvider } from "../contexts/GlassModuleProvider";
 import { CallsHistoryPage } from "./calls-history/CallsHistoryPage";
 import { CountryInformationPage } from "./country-information/CountryInformationPage";
 import { CurrentCallPage } from "./current-call/CurrentCallPage";
 import { DataSubmissionPage } from "./data-submission/DataSubmissionPage";
-import { FakeLandingPage } from "./landing/FakeLandingPage";
+import { LandingPage } from "./landing/LandingPage";
 import { UploadHistoryPage } from "./upload-history/UploadHistoryPage";
 
 export const Router: React.FC = React.memo(() => {
     return (
         <HashRouter>
             <Switch>
-                {/* <Route
-                    path="/for/:name?"
-                    render={({ match }) => <ExamplePage name={match.params.name ?? "Stranger"} />}
-                />
+                
+                <GlassModuleContextProvider>
+                    <Route
+                        path="/current-call/:module"
+                        render={({ match }) => <CurrentCallPage moduleName={match.params.module} />}
+                    />
+                    <Route
+                        path="/data-submission/:module"
+                        render={({ match }) => <DataSubmissionPage moduleName={match.params.module} />}
+                    />
+                    <Route path="/calls-history/:module/:orgUnit" component={CallsHistoryPage} />
 
-          
-                <Route render={() => <LandingPage />} /> */}
-
-                {/* Default route */}
-                <Route
-                    path="/current-call/:module"
-                    render={({ match }) => <CurrentCallPage moduleName={match.params.module} />}
-                />
-                <Route
-                    path="/data-submission/:module"
-                    render={({ match }) => <DataSubmissionPage moduleName={match.params.module} />}
-                />
-                <Route
-                    path="/calls-history/:module"
-                    render={({ match }) => <CallsHistoryPage moduleName={match.params.module} />}
-                />
-                <Route
-                    path="/upload-history/:module"
-                    render={({ match }) => <UploadHistoryPage moduleName={match.params.module} />}
-                />
-                <Route
-                    path="/country-information/:module"
-                    render={({ match }) => <CountryInformationPage moduleName={match.params.module} />}
-                />
-
-                <Route render={() => <FakeLandingPage />} />
+                    <Route
+                        path="/upload-history/:module"
+                        render={({ match }) => <UploadHistoryPage moduleName={match.params.module} />}
+                    />
+                    <Route
+                        path="/country-information/:module"
+                        render={({ match }) => <CountryInformationPage moduleName={match.params.module} />}
+                    />
+                </GlassModuleContextProvider>
+                <Route render={() => <LandingPage />} />
             </Switch>
         </HashRouter>
     );
