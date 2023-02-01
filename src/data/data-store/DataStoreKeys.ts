31fe8184f04c6631360d9614a8db1e37c3eae217
Sneha N <9sneha.n@gmail.com>
2023-02-02 00:28:01 +0530

    update key in datastore

--- src/data/data-store/DataStoreKeys.ts
+++ src/data/data-store/DataStoreKeys.ts
@@ -2,5 +2,5 @@ export const DataStoreKeys = {
     MODULES: "modules",
     NEWS: "news",
     DATA_SUBMISSIONS: "data-submissions",
-    UPLOADS: "submissions", //TO DO : Update in datastore
+    UPLOADS: "uploads",
 };
