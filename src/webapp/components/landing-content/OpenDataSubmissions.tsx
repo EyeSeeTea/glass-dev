dadf6b31ab38aa508c3f2957116a85cdb78a59ce
Sneha N <9sneha.n@gmail.com>
2023-02-22 12:47:38 +0530

    Do not show loader for each card module

--- src/webapp/components/landing-content/OpenDataSubmissions.tsx
+++ src/webapp/components/landing-content/OpenDataSubmissions.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { ModuleCard } from "../module-card/ModuleCard";
 import i18n from "@eyeseetea/d2-ui-components/locales";
 import { useOpenDataSubmissionsByOrgUnit } from "../../hooks/useOpenDataSubmissionsByOrgUnit";
@@ -7,9 +7,11 @@ import { useCurrentOrgUnitContext } from "../../contexts/current-orgUnit-context
 import { useAppContext } from "../../contexts/app-context";
 import { ContentLoader } from "../content-loader/ContentLoader";
 import styled from "styled-components";
+import { useGlassModules } from "../../hooks/useGlassModules";
 
 export const OpenDataSubmissions: React.FC = () => {
     const { compositionRoot } = useAppContext();
+    const modules = useGlassModules(compositionRoot);
     const orgUnit = useCurrentOrgUnitContext();
     const openDataSubmissions = useOpenDataSubmissionsByOrgUnit(
         compositionRoot,
@@ -20,19 +22,26 @@ export const OpenDataSubmissions: React.FC = () => {
             <Grid item xs={12}>
                 <h2 className="section-title">Open Data Submissions</h2>
             </Grid>
-            {openDataSubmissions.kind === "loaded" && openDataSubmissions.data.length ? (
-                openDataSubmissions.data.map(openDataSubmission => (
-                    <Grid item xs={6} key={openDataSubmission.id}>
-                        <ModuleCard moduleId={openDataSubmission.module} period={openDataSubmission.period} />
-                    </Grid>
-                ))
-            ) : (
-                <StyledNoData>{i18n.t("No Open Data Submissions ...")}</StyledNoData>
-            )}
+            <ContentLoader content={modules}>
+                {openDataSubmissions.kind === "loaded" &&
+                modules.kind === "loaded" &&
+                openDataSubmissions.data.length ? (
+                    openDataSubmissions.data.map(openDataSubmission => {
+                        const cardModule = modules.data.find(m => m.id === openDataSubmission.module);
+                        return (
+                            <Grid item xs={6} key={openDataSubmission.id}>
+                                {cardModule && <ModuleCard period={openDataSubmission.period} module={cardModule} />}
+                            </Grid>
+                        );
+                    })
+                ) : (
+                    <StyledNoData>{i18n.t("No Open Data Submissions")}</StyledNoData>
+                )}
+            </ContentLoader>
         </ContentLoader>
     );
 };
 
-const StyledNoData = styled.p`
+const StyledNoData = styled(Typography)`
     padding-left: 20px;
 `;
