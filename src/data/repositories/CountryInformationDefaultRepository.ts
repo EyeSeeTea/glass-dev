2e355a0975efb557a3dd26fffec0fbd3a45b7e6f
xurxodev <xurxosf@gmail.com>
2023-02-24 12:14:27 +0100

    Refactor NationalFocalPoint to dynamic values: - Values according to data element sort order in program stage

--- src/data/repositories/CountryInformationDefaultRepository.ts
+++ src/data/repositories/CountryInformationDefaultRepository.ts
@@ -1,4 +1,4 @@
-import { D2Api } from "@eyeseetea/d2-api/2.34";
+import { D2Api, MetadataPick } from "@eyeseetea/d2-api/2.34";
 import { Event as ProgramEvent } from "@eyeseetea/d2-api/api/events";
 import { CountryInformation } from "../../domain/entities/CountryInformation";
 import { Future, FutureData } from "../../domain/entities/Future";
@@ -10,17 +10,6 @@ import { Instance } from "../entities/Instance";
 const ARMFocalPointProgram = "oo0bqS0AqMI";
 const moduleAttribute = "Fh6atHPjdxC";
 
-const familyNameDE = "GXDSEwgeGwK";
-const firstNameDE = "Z9fUYlB1HmQ";
-const functionNameDE = "EVKMO195LMe";
-const emailAddressDE = "CgCPexj0q43";
-const preferredLanguageDE = "h6okeC9Fh0r";
-const telephoneDE = "I2UOcdFIvUq";
-const institutionDE = "RM1V3aPr5Lc";
-const institutionAddressDE = "YJEnWwlhx78";
-const cityDE = "Fc2Zy5r7Hz4";
-const zipCodeDE = "Kx1ErRIlfYi";
-
 export class CountryInformationDefaultRepository implements CountryInformationRepository {
     private api: D2Api;
 
@@ -30,62 +19,78 @@ export class CountryInformationDefaultRepository implements CountryInformationRe
 
     get(countryId: string, module: string): FutureData<CountryInformation> {
         return Future.joinObj({
-            tei: apiToFuture(
-                this.api.get<D2TEIsResponse>("/trackedEntityInstances", {
-                    program: ARMFocalPointProgram,
-                    ou: [countryId],
-                    fields: "*",
-                    totalPages: true,
-                    page: 1,
-                    pageSize: 1,
-                    filter: `${moduleAttribute}:eq:${module}`,
-                })
-            ),
-            orgUnits: apiToFuture(
-                this.api.models.organisationUnits.get({
-                    id: countryId,
-                    fields: {
-                        id: true,
-                        shortName: true,
-                        level: true,
-                    },
-                    includeAncestors: true,
-                })
-            ),
-        }).map(({ tei, orgUnits }) => {
-            const TEI = tei.trackedEntityInstances[0];
-            const enrollment = TEI?.enrollments[0];
-            const events = TEI?.enrollments[0]?.events;
+            program: this.getProgram(),
+            tei: this.getTEI(countryId, module),
+            orgUnits: this.getOrgUnits(countryId),
+        }).map(({ program, tei, orgUnits }) => {
+            const countryLevel = orgUnits.find(ou => ou.id === countryId)?.level || 0;
+            const countryName = orgUnits.find(ou => ou.id === countryId)?.shortName || "";
+            const regionName =
+                orgUnits.find(ou => ou.id !== countryId && ou.level === countryLevel - 1)?.shortName || "";
 
-            const region = orgUnits.objects.find(ou => ou.id !== countryId)?.shortName || "";
-            const country = orgUnits.objects.find(ou => ou.id === countryId)?.shortName || "";
+            const enrollment = tei?.enrollments[0];
+            const events = tei?.enrollments[0]?.events || [];
+            const programstageDataElements = program?.programStages[0]?.programStageDataElements || [];
 
             return {
-                WHORegion: region,
-                country: country,
+                WHORegion: regionName,
+                country: countryName,
                 year: new Date().getFullYear(),
                 enrolmentStatus: enrollment?.status || "",
                 enrolmentDate: enrollment?.enrollmentDate || "",
                 nationalFocalPoints:
-                    events?.map(event => {
+                    events.map((event: ProgramEvent) => {
                         return {
                             id: event.event,
-                            familyName: event.dataValues.find(dv => dv.dataElement === familyNameDE)?.value || "",
-                            firstName: event.dataValues.find(dv => dv.dataElement === firstNameDE)?.value || "",
-                            function: event.dataValues.find(dv => dv.dataElement === functionNameDE)?.value || "",
-                            emailAddress: event.dataValues.find(dv => dv.dataElement === emailAddressDE)?.value || "",
-                            preferredLanguage:
-                                event.dataValues.find(dv => dv.dataElement === preferredLanguageDE)?.value || "",
-                            telephone: event.dataValues.find(dv => dv.dataElement === telephoneDE)?.value || "",
-                            institution: event.dataValues.find(dv => dv.dataElement === institutionDE)?.value || "",
-                            institutionAddress:
-                                event.dataValues.find(dv => dv.dataElement === institutionAddressDE)?.value || "",
-                            city: event.dataValues.find(dv => dv.dataElement === cityDE)?.value || "",
-                            zipCode: event.dataValues.find(dv => dv.dataElement === zipCodeDE)?.value || "",
+                            values: programstageDataElements.map(programStageDataElement => {
+                                const dataValue = event.dataValues.find(
+                                    dv => dv.dataElement === programStageDataElement.dataElement.id
+                                );
+
+                                return {
+                                    id: programStageDataElement.dataElement.id,
+                                    name: programStageDataElement.dataElement.shortName,
+                                    value: dataValue?.value || "",
+                                };
+                            }),
                         };
                     }) || [],
             };
         });
+    }
+
+    private getOrgUnits(countryId: string): FutureData<D2OrgUnit[]> {
+        return apiToFuture(
+            this.api.get<D2OrgUnitsResponse>(`/organisationUnits/${countryId}`, {
+                fields: Object.keys(orgUnitFields).join(","),
+                includeAncestors: true,
+            })
+        ).map(response => response.organisationUnits);
+    }
+
+    private getTEI(countryId: string, module: string): FutureData<D2TEI | undefined> {
+        return apiToFuture(
+            this.api.get<D2TEIsResponse>("/trackedEntityInstances", {
+                program: ARMFocalPointProgram,
+                ou: [countryId],
+                fields: "*",
+                totalPages: true,
+                page: 1,
+                pageSize: 1,
+                filter: `${moduleAttribute}:eq:${module}`,
+                order: "created:Desc",
+            })
+        ).map(response => response.trackedEntityInstances[0]);
+    }
+
+    private getProgram(): FutureData<D2Program | undefined> {
+        return apiToFuture(
+            this.api.models.programs.get({
+                fields: programFields,
+                includeAncestors: true,
+                filter: { id: { eq: ARMFocalPointProgram } },
+            })
+        ).map(response => response.objects[0]);
     }
 }
 
@@ -112,3 +117,37 @@ export interface Enrollment {
     status: "ACTIVE" | "COMPLETED" | "CANCELED";
     events: ProgramEvent[];
 }
+
+const programFields = {
+    id: true,
+    programStages: {
+        programStageDataElements: {
+            id: true,
+            sortOrder: true,
+            dataElement: { id: true, shortName: true },
+        },
+    },
+} as const;
+
+type D2Program = MetadataPick<{
+    programs: { fields: typeof programFields };
+}>["programs"][number];
+
+export interface D2OrgUnitsResponse {
+    organisationUnits: D2OrgUnit[];
+    pager: {
+        pageSize: number;
+        total: number;
+        page: number;
+    };
+}
+
+const orgUnitFields = {
+    id: true,
+    shortName: true,
+    level: true,
+} as const;
+
+type D2OrgUnit = MetadataPick<{
+    organisationUnits: { fields: typeof orgUnitFields };
+}>["organisationUnits"][number];
--- src/domain/entities/CountryInformation.ts
+++ src/domain/entities/CountryInformation.ts
@@ -9,14 +9,11 @@ export interface CountryInformation {
 
 export interface NationalFocalPoint {
     id: string;
-    familyName: string;
-    firstName: string;
-    function: string;
-    emailAddress: string;
-    preferredLanguage: string;
-    telephone: string;
-    institution: string;
-    institutionAddress: string;
-    city: string;
-    zipCode: string;
+    values: NationalFocalPointValue[];
+}
+
+export interface NationalFocalPointValue {
+    id: string;
+    name: string;
+    value: string;
 }
