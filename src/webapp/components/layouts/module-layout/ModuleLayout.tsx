614a4102bbadad678e92011bcf7b06f91a96ad49
xurxodev <xurxosf@gmail.com>
2023-02-28 13:40:07 +0100

    On error from ModuleLayout navigate to home

--- src/webapp/components/layouts/module-layout/ModuleLayout.tsx
+++ src/webapp/components/layouts/module-layout/ModuleLayout.tsx
@@ -4,15 +4,24 @@ import { MainLayout } from "../main-layout/MainLayout";
 import { useGlassModule } from "../../../hooks/useGlassModule";
 import { useAppContext } from "../../../contexts/app-context";
 import { ContentLoader } from "../../content-loader/ContentLoader";
+import { useHistory } from "react-router-dom";
+import { useCurrentModuleContext } from "../../../contexts/current-module-context";
 
 export const ModuleLayout: React.FC = ({ children }) => {
     const { compositionRoot } = useAppContext();
+    const history = useHistory();
+    const { resetCurrentModuleAccess } = useCurrentModuleContext();
 
     const module = useGlassModule(compositionRoot);
 
+    const onError = () => {
+        resetCurrentModuleAccess();
+        history.push("/");
+    };
+
     return (
         <MainLayout>
-            <ContentLoader content={module} showErrorAsSnackbar={true}>
+            <ContentLoader content={module} showErrorAsSnackbar={true} onError={onError}>
                 {children}
             </ContentLoader>
         </MainLayout>
