ad92eb9a8525f15c8e91a3437cec02e1fe2e55e3
davidbgomes <davidbgomes92@gmail.com>
2023-03-03 13:36:33 +0000

    Start layout fixes & add questionnaires info

--- src/webapp/components/current-data-submission/overview/CurrentStatus.tsx
+++ src/webapp/components/current-data-submission/overview/CurrentStatus.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import { Box } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Box, Paper, Table, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 import styled from "styled-components";
 import i18n from "@eyeseetea/d2-ui-components/locales";
 import { CtaButtons } from "./CtaButtons";
 import { StatusCTAs } from "./StatusDetails";
+import { useAppContext } from "../../../contexts/app-context";
+import { useCurrentModuleContext } from "../../../contexts/current-module-context";
+import { useCurrentOrgUnitContext } from "../../../contexts/current-orgUnit-context";
+import { useLocation } from "react-router-dom";
+import { QuestionnaireBase } from "../../../../domain/entities/Questionnaire";
 
 interface StatusProps {
     moduleName: string;
@@ -11,9 +16,41 @@ interface StatusProps {
     description: string;
     statusColor: string;
     ctas: StatusCTAs[];
+    showUploadHistory: boolean;
 }
 
-export const CurrentStatus: React.FC<StatusProps> = ({ title, description, statusColor, ctas }) => {
+export const CurrentStatus: React.FC<StatusProps> = ({ title, description, statusColor, ctas, showUploadHistory }) => {
+    const { compositionRoot } = useAppContext();
+    const location = useLocation();
+    const {
+        currentModuleAccess: { moduleId },
+    } = useCurrentModuleContext();
+    const {
+        currentOrgUnitAccess: { orgUnitId },
+    } = useCurrentOrgUnitContext();
+
+    const [questionnaires, setQuestionnaires] = useState<QuestionnaireBase[]>();
+
+    const queryParameters = new URLSearchParams(location.search);
+    const periodFromUrl = parseInt(queryParameters.get("period") || "");
+    const year = periodFromUrl || new Date().getFullYear() - 1;
+
+    useEffect(() => {
+        if (showUploadHistory) {
+            compositionRoot.glassModules.getById(moduleId).run(
+                currentModule => {
+                    compositionRoot.questionnaires.getList(currentModule, { orgUnitId, year }).run(
+                        questionnairesData => {
+                            setQuestionnaires(questionnairesData);
+                        },
+                        () => {}
+                    );
+                },
+                () => {}
+            );
+        }
+    }, []);
+
     return (
         <div>
             <Box sx={{ m: 2 }} />
@@ -23,6 +60,29 @@ export const CurrentStatus: React.FC<StatusProps> = ({ title, description, statu
             </div>
             <Box sx={{ m: 2 }} />
             <StyledDescription>{i18n.t(description)}</StyledDescription>
+            {showUploadHistory && (
+                <TableContainer component={Paper} style={{ marginTop: "10px" }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>{i18n.t("Content")}</TableCell>
+                                <TableCell>{i18n.t("Mandatory")}</TableCell>
+                                <TableCell>{i18n.t("State")}</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableRow>
+                            <TableCell>{`Up to 6 datasets`}</TableCell>
+                            <TableCell>{`No`}</TableCell>
+                            <TableCell>{`0 uploaded`}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>{`${questionnaires?.length} Questionnaires`}</TableCell>
+                            <TableCell>{`${questionnaires?.[0]?.isMandatory ? "Yes" : "No"}`}</TableCell>
+                            <TableCell>{`${questionnaires?.filter(el => el.isCompleted).length} completed`}</TableCell>
+                        </TableRow>
+                    </Table>
+                </TableContainer>
+            )}
             <CtaButtons ctas={ctas} />
         </div>
     );
