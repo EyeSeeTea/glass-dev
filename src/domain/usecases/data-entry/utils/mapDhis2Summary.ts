c0548257297ef1a18ca974a9111ee6258aac984a
xurxodev <xurxosf@gmail.com>
2023-03-03 10:18:18 +0100

    Add importCounts to import summary

--- src/domain/usecases/data-entry/utils/mapDhis2Summary.ts
+++ src/domain/usecases/data-entry/utils/mapDhis2Summary.ts
@@ -1,10 +1,10 @@
 import { DataValuesSaveSummary } from "../../../entities/data-entry/DataValuesSaveSummary";
 import { ImportSummary } from "../../../entities/data-entry/ImportSummary";
 
-export function mapToImportSummary(datasetImportStatus: DataValuesSaveSummary): ImportSummary {
+export function mapToImportSummary(dhis2Summary: DataValuesSaveSummary): ImportSummary {
     const nonBlockingErrors =
-        datasetImportStatus.status === "WARNING"
-            ? datasetImportStatus.conflicts?.map(status => {
+        dhis2Summary.status === "WARNING"
+            ? dhis2Summary.conflicts?.map(status => {
                   return {
                       error: status.value,
                       count: 1,
@@ -13,8 +13,8 @@ export function mapToImportSummary(datasetImportStatus: DataValuesSaveSummary): 
             : [];
 
     const blokingErrors =
-        datasetImportStatus.status === "ERROR"
-            ? datasetImportStatus.conflicts?.map(status => {
+        dhis2Summary.status === "ERROR"
+            ? dhis2Summary.conflicts?.map(status => {
                   return {
                       error: status.value,
                       count: 1,
@@ -24,15 +24,15 @@ export function mapToImportSummary(datasetImportStatus: DataValuesSaveSummary): 
 
     const finalBlockingErrors = _.compact([
         ...blokingErrors,
-        datasetImportStatus.importCount.ignored > 0
+        dhis2Summary.importCount.ignored > 0
             ? {
                   error: "Import Ignored",
-                  count: datasetImportStatus.importCount.ignored,
+                  count: dhis2Summary.importCount.ignored,
               }
             : undefined,
     ]);
 
     const status = finalBlockingErrors.length > 0 ? "ERROR" : nonBlockingErrors.length > 0 ? "WARNING" : "SUCCESS";
 
-    return { status, nonBlockingErrors, blockingErrors: finalBlockingErrors };
+    return { status, nonBlockingErrors, blockingErrors: finalBlockingErrors, importCount: dhis2Summary.importCount };
 }
