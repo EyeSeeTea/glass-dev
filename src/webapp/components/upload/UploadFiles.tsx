41fc053ec59b355b16277ddfe706d86997753b87
xurxodev <xurxosf@gmail.com>
2023-03-03 13:27:57 +0100

    Rename value for data sets

--- src/webapp/components/upload/UploadFiles.tsx
+++ src/webapp/components/upload/UploadFiles.tsx
@@ -20,27 +20,27 @@ interface UploadFilesProps {
 const datasetOptions = [
     {
         label: "Dataset 1",
-        value: "1",
+        value: "DS1",
     },
     {
         label: "Dataset 2",
-        value: "2",
+        value: "DS2",
     },
     {
         label: "Dataset 3",
-        value: "3",
+        value: "DS3",
     },
     {
         label: "Dataset 4",
-        value: "4",
+        value: "DS4",
     },
     {
         label: "Dataset 5",
-        value: "5",
+        value: "DS5",
     },
     {
         label: "Dataset 6",
-        value: "6",
+        value: "DS6",
     },
 ];
 
