5ea689daf173e06aaebeca240d60ba2033c315dc
Sneha N <9sneha.n@gmail.com>
2023-03-08 22:30:28 +0530

    Transition status from COMPLETE to PENDING APPROVAL on user confirmation

--- src/webapp/components/current-data-submission/overview/CtaButtons.tsx
+++ src/webapp/components/current-data-submission/overview/CtaButtons.tsx
@@ -1,17 +1,64 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React, { Dispatch, SetStateAction } from "react";
+import { Button, CircularProgress, DialogActions, DialogContent, Typography } from "@material-ui/core";
 import styled from "styled-components";
 import i18n from "@eyeseetea/d2-ui-components/locales";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { StatusCTAs } from "./StatusDetails";
 import { useGlassCaptureAccess } from "../../../hooks/useGlassCaptureAccess";
+import { ConfirmationDialog } from "@eyeseetea/d2-ui-components";
+import { useAppContext } from "../../../contexts/app-context";
+import { useCurrentDataSubmissionId } from "../../../hooks/useCurrentDataSubmissionId";
+import { useCurrentOrgUnitContext } from "../../../contexts/current-orgUnit-context";
+import { useCurrentModuleContext } from "../../../contexts/current-module-context";
+import { DataSubmissionStatusTypes } from "../../../../domain/entities/GlassDataSubmission";
 
 export interface CtaButtonsProps {
     ctas: StatusCTAs[];
+    setRefetchStatus: Dispatch<SetStateAction<DataSubmissionStatusTypes>>;
 }
 
-export const CtaButtons: React.FC<CtaButtonsProps> = ({ ctas }) => {
+export const CtaButtons: React.FC<CtaButtonsProps> = ({ ctas, setRefetchStatus }) => {
+    const { compositionRoot } = useAppContext();
     const hasCurrentUserCaptureAccess = useGlassCaptureAccess();
+    const [open, setOpen] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const { currentOrgUnitAccess } = useCurrentOrgUnitContext();
+    const { currentModuleAccess } = useCurrentModuleContext();
+    const location = useLocation();
+    const queryParameters = new URLSearchParams(location.search);
+    const periodFromUrl = parseInt(queryParameters.get("period") || "");
+    const year = periodFromUrl || new Date().getFullYear() - 1;
+
+    const dataSubmissionId = useCurrentDataSubmissionId(
+        compositionRoot,
+        currentModuleAccess.moduleId,
+        currentOrgUnitAccess.orgUnitId,
+        year
+    );
+
+    const showConfirmationDialog = () => {
+        setOpen(true);
+    };
+    const hideConfirmationDialog = () => {
+        setOpen(false);
+    };
+    const updateDataSubmissionStatus = () => {
+        setIsLoading(true);
+        compositionRoot.glassDataSubmission.setStatus(dataSubmissionId, "PENDING_APPROVAL").run(
+            () => {
+                //Triggerring relaod of status in parent
+                setRefetchStatus("PENDING_APPROVAL");
+                setIsLoading(false);
+                setOpen(false);
+            },
+            () => {
+                setIsLoading(false);
+                setOpen(false);
+            }
+        );
+    };
+
     const getCTAButton = (cta: StatusCTAs) => {
         // TODO : Button click event handlers to be added as corresponding feature developed.
         switch (cta) {
@@ -31,7 +78,7 @@ export const CtaButtons: React.FC<CtaButtonsProps> = ({ ctas }) => {
                 );
             case "Send to WHO for revision":
                 return (
-                    <Button variant="contained" color="primary" key={2}>
+                    <Button variant="contained" color="primary" key={2} onClick={showConfirmationDialog}>
                         {i18n.t("Send to WHO for revision")}
                     </Button>
                 );
@@ -54,9 +101,29 @@ export const CtaButtons: React.FC<CtaButtonsProps> = ({ ctas }) => {
 
     return (
         <ContentWrapper>
-            {ctas.map(cta => {
-                return getCTAButton(cta);
-            })}
+            <ConfirmationDialog
+                isOpen={open}
+                title={i18n.t("Confirm submission")}
+                onSave={updateDataSubmissionStatus}
+                onCancel={hideConfirmationDialog}
+                saveText={i18n.t("Ok")}
+                cancelText={i18n.t("Cancel")}
+                fullWidth={true}
+                disableEnforceFocus
+            >
+                <DialogContent>
+                    <Typography>
+                        Please review that the submission package contains all the datasets that you want to include.
+                        After you submit this package, you won’t be able to edit it anymore wihout WHO permissions
+                    </Typography>
+                </DialogContent>
+                <DialogActions>{isLoading && <CircularProgress size={25} />}</DialogActions>
+            </ConfirmationDialog>
+            <>
+                {ctas.map(cta => {
+                    return getCTAButton(cta);
+                })}
+            </>
         </ContentWrapper>
     );
 };
