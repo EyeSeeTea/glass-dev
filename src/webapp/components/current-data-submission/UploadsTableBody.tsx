0845811215433cb7ad35846e94d6bfda7fe6e54b
Sneha N <9sneha.n@gmail.com>
2023-03-10 14:26:04 +0530

    Delete upload and document from datastore

--- src/webapp/components/current-data-submission/UploadsTableBody.tsx
+++ src/webapp/components/current-data-submission/UploadsTableBody.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TableBody, TableCell, TableRow, Button } from "@material-ui/core";
 import styled from "styled-components";
 import i18n from "@eyeseetea/d2-ui-components/locales";
@@ -6,13 +6,19 @@ import dayjs from "dayjs";
 import { UploadsDataItem } from "../../entities/uploads";
 import { CloudDownloadOutlined, DeleteOutline } from "@material-ui/icons";
 import { useAppContext } from "../../contexts/app-context";
+import { useSnackbar } from "@eyeseetea/d2-ui-components";
+import { CircularProgress } from "material-ui";
 
 export interface UploadsTableBodyProps {
     rows?: UploadsDataItem[];
+    refreshUploads: React.Dispatch<React.SetStateAction<{}>>;
 }
 
-export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows }) => {
+export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows, refreshUploads }) => {
     const { compositionRoot } = useAppContext();
+    const snackbar = useSnackbar();
+    const [loading, setLoading] = useState<boolean>(false);
+
     const downloadFile = (fileId: string, fileName: string) => {
         compositionRoot.glassDocuments.download(fileId).run(
             file => {
@@ -27,8 +33,33 @@ export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows }) => {
             () => {}
         );
     };
+
+    const deleteDataset = (uploadId: string, fileId: string, fileName: string) => {
+        //Deleting a dataset completely has the following steps:
+        //1. Delete corresponding 'upload' and 'document' from Datastore
+        //2. Delete corresponding file from DHIS
+        //3. Delete corresponsding datasetValue for each row in the file.
+        setLoading(true);
+        compositionRoot.glassDocuments.deleteByUploadId(uploadId).run(
+            _data => {
+                refreshUploads({}); //Trigger re-render of parent
+                setLoading(false);
+            },
+            errorMessage => {
+                snackbar.error(errorMessage);
+                setLoading(false);
+            }
+        );
+    };
     return (
         <>
+            {loading && (
+                <TableRow>
+                    <TableCell>
+                        <CircularProgress size={25} />
+                    </TableCell>
+                </TableRow>
+            )}
             {rows && rows.length ? (
                 <StyledTableBody>
                     {rows.map((row: UploadsDataItem) => (
@@ -45,7 +76,7 @@ export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows }) => {
                                 </Button>
                             </TableCell>
                             <TableCell>
-                                <Button>
+                                <Button onClick={() => deleteDataset(row.id, row.fileId, row.fileName)}>
                                     <DeleteOutline />
                                 </Button>
                             </TableCell>
