751366d940a3305f843d2c561e691c8c30f0e292
Sneha N <9sneha.n@gmail.com>
2023-03-16 13:31:06 +0530

    Delete both Sample and Ris files of a given dataset

--- src/webapp/components/current-data-submission/UploadsTableBody.tsx
+++ src/webapp/components/current-data-submission/UploadsTableBody.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { TableBody, TableCell, TableRow, Button } from "@material-ui/core";
+import { TableBody, TableCell, TableRow, Button, DialogContent, Typography, DialogActions } from "@material-ui/core";
 import styled from "styled-components";
 import i18n from "@eyeseetea/d2-ui-components/locales";
 import dayjs from "dayjs";
 import { UploadsDataItem } from "../../entities/uploads";
 import { CloudDownloadOutlined, DeleteOutline } from "@material-ui/icons";
 import { useAppContext } from "../../contexts/app-context";
-import { useSnackbar } from "@eyeseetea/d2-ui-components";
+import { ConfirmationDialog, useSnackbar } from "@eyeseetea/d2-ui-components";
 import { CircularProgress } from "material-ui";
 
 export interface UploadsTableBodyProps {
@@ -18,6 +18,16 @@ export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows, refres
     const { compositionRoot } = useAppContext();
     const snackbar = useSnackbar();
     const [loading, setLoading] = useState<boolean>(false);
+    const [open, setOpen] = React.useState(false);
+    const [rowToDelete, setRowToDelete] = useState<UploadsDataItem>();
+
+    const showConfirmationDialog = (rowToDelete: UploadsDataItem) => {
+        setRowToDelete(rowToDelete);
+        setOpen(true);
+    };
+    const hideConfirmationDialog = () => {
+        setOpen(false);
+    };
 
     const downloadFile = (fileId: string, fileName: string) => {
         compositionRoot.glassDocuments.download(fileId).run(
@@ -34,65 +44,129 @@ export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows, refres
         );
     };
 
-    const deleteDataset = (
-        uploadId: string,
-        fileId: string,
-        fileName: string,
-        fileType: string,
-        batchId: string,
-        period: string
-    ) => {
+    const deleteRisFile = (uploadId: string, file: Blob, fileName: string, batchId: string, period: string) => {
+        const risFile = new File([file], fileName);
+        compositionRoot.dataSubmision.RISFile(risFile, batchId, parseInt(period), "DELETES").run(
+            summary => {
+                snackbar.info(`${summary.importCount.deleted} records deleted.`);
+                compositionRoot.glassDocuments.deleteByUploadId(uploadId).run(
+                    _data => {
+                        refreshUploads({}); //Trigger re-render of parent
+                        setLoading(false);
+                        hideConfirmationDialog();
+                    },
+                    errorMessage => {
+                        snackbar.error(errorMessage);
+                        setLoading(false);
+                        hideConfirmationDialog();
+                    }
+                );
+            },
+            error => {
+                snackbar.error(error);
+            }
+        );
+    };
+
+    const deleteSampleFile = (uploadId: string, file: Blob, fileName: string, batchId: string, period: string) => {
+        const sampleFile = new File([file], fileName);
+        compositionRoot.dataSubmision.sampleFile(sampleFile, batchId, parseInt(period), "DELETES").run(
+            summary => {
+                snackbar.info(`${summary.importCount.deleted} records deleted.`);
+                compositionRoot.glassDocuments.deleteByUploadId(uploadId).run(
+                    _data => {
+                        refreshUploads({}); //Trigger re-render of parent
+                        setLoading(false);
+                        hideConfirmationDialog();
+                    },
+                    errorMessage => {
+                        snackbar.error(errorMessage);
+                        setLoading(false);
+                        hideConfirmationDialog();
+                    }
+                );
+            },
+            error => {
+                snackbar.error(error);
+            }
+        );
+    };
+
+    const deleteDataset = () => {
         //Deleting a dataset completely has the following steps:
         //1. Delete corresponsding datasetValue for each row in the file.
         //2. Delete corresponding document from DHIS
         //3. Delete corresponding 'upload' and 'document' from Datastore
-        setLoading(true);
-        compositionRoot.glassDocuments.download(fileId).run(
-            file => {
-                if (fileType === "RIS") {
-                    const risFile = new File([file], fileName);
-                    compositionRoot.dataSubmision.RISFile(risFile, batchId, parseInt(period), "DELETES").run(
-                        summary => {
-                            snackbar.info(`${summary.importCount.deleted} records deleted.`);
-                            compositionRoot.glassDocuments.deleteByUploadId(uploadId).run(
-                                _data => {
-                                    refreshUploads({}); //Trigger re-render of parent
-                                    setLoading(false);
+        if (rowToDelete) {
+            setLoading(true);
+
+            compositionRoot.glassDocuments.download(rowToDelete.fileId).run(
+                file => {
+                    if (rowToDelete.fileType === "RIS") {
+                        deleteRisFile(
+                            rowToDelete?.id,
+                            file,
+                            rowToDelete.fileName,
+                            rowToDelete.batchId,
+                            rowToDelete.period
+                        );
+                        //check if corresponding sample file is uploaded, delete that too.
+                        const correspondingSample = rows
+                            ?.filter(
+                                upload =>
+                                    upload.batchId === rowToDelete.batchId &&
+                                    upload.fileType === "SAMPLE" &&
+                                    upload.period === rowToDelete.period
+                            )
+                            ?.at(0);
+                        if (correspondingSample)
+                            compositionRoot.glassDocuments.download(correspondingSample.fileId).run(
+                                sampleFile => {
+                                    deleteSampleFile(
+                                        correspondingSample.id,
+                                        sampleFile,
+                                        correspondingSample.fileName,
+                                        rowToDelete.batchId,
+                                        rowToDelete.period
+                                    );
                                 },
-                                errorMessage => {
-                                    snackbar.error(errorMessage);
-                                    setLoading(false);
-                                }
+                                () => {}
                             );
-                        },
-                        error => {
-                            snackbar.error(error);
-                        }
-                    );
-                } else if (fileType === "SAMPLE") {
-                    const sampleFile = new File([file], fileName);
-                    compositionRoot.dataSubmision.sampleFile(sampleFile, batchId, parseInt(period), "DELETES").run(
-                        summary => {
-                            snackbar.info(`${summary.importCount.deleted} records deleted.`);
-                            compositionRoot.glassDocuments.deleteByUploadId(uploadId).run(
-                                _data => {
-                                    refreshUploads({}); //Trigger re-render of parent
-                                    setLoading(false);
+                    } else if (rowToDelete.fileType === "SAMPLE") {
+                        deleteSampleFile(
+                            rowToDelete.id,
+                            file,
+                            rowToDelete.fileName,
+                            rowToDelete.batchId,
+                            rowToDelete.period
+                        );
+                        //check if corresponding RIS file is uploaded, delete that too.
+                        const correspondingRIS = rows
+                            ?.filter(
+                                upload =>
+                                    upload.batchId === rowToDelete.batchId &&
+                                    upload.fileType === "RIS" &&
+                                    upload.period === rowToDelete.period
+                            )
+                            ?.at(0);
+                        if (correspondingRIS)
+                            compositionRoot.glassDocuments.download(correspondingRIS.fileId).run(
+                                risFile => {
+                                    deleteRisFile(
+                                        correspondingRIS.id,
+                                        risFile,
+                                        correspondingRIS.fileName,
+                                        rowToDelete.batchId,
+                                        rowToDelete.period
+                                    );
                                 },
-                                errorMessage => {
-                                    snackbar.error(errorMessage);
-                                    setLoading(false);
-                                }
+                                () => {}
                             );
-                        },
-                        error => {
-                            snackbar.error(error);
-                        }
-                    );
-                }
-            },
-            () => {}
-        );
+                    }
+                },
+                () => {}
+            );
+        }
     };
     return (
         <>
@@ -103,6 +177,25 @@ export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows, refres
                     </TableCell>
                 </TableRow>
             )}
+            <ConfirmationDialog
+                isOpen={open}
+                title={i18n.t("Confirm Delete")}
+                onSave={deleteDataset}
+                onCancel={hideConfirmationDialog}
+                saveText={i18n.t("Ok")}
+                cancelText={i18n.t("Cancel")}
+                fullWidth={true}
+                disableEnforceFocus
+            >
+                <DialogContent>
+                    <Typography>
+                        {i18n.t(
+                            "Deleting this upload will delete both SAMPLE and RIS files for the given dataset. Are you sure you want to delete?"
+                        )}
+                    </Typography>
+                </DialogContent>
+                <DialogActions>{loading && <CircularProgress size={25} />}</DialogActions>
+            </ConfirmationDialog>
             {rows && rows.length ? (
                 <StyledTableBody>
                     {rows.map((row: UploadsDataItem) => (
@@ -119,18 +212,7 @@ export const UploadsTableBody: React.FC<UploadsTableBodyProps> = ({ rows, refres
                                 </Button>
                             </TableCell>
                             <TableCell>
-                                <Button
-                                    onClick={() =>
-                                        deleteDataset(
-                                            row.id,
-                                            row.fileId,
-                                            row.fileName,
-                                            row.fileType,
-                                            row.batchId,
-                                            row.period
-                                        )
-                                    }
-                                >
+                                <Button onClick={() => showConfirmationDialog(row)}>
                                     <DeleteOutline />
                                 </Button>
                             </TableCell>
