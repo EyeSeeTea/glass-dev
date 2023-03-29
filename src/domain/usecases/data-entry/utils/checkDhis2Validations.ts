2590c6ce367c3045a6a86673ac853cdc1d1e45ba
davidbgomes <davidbgomes92@gmail.com>
2023-03-29 10:41:59 +0100

    fix dhis2 undefined validation

--- src/domain/usecases/data-entry/utils/checkDhis2Validations.ts
+++ src/domain/usecases/data-entry/utils/checkDhis2Validations.ts
@@ -5,15 +5,18 @@ import { ConsistencyError } from "../../../entities/data-entry/ImportSummary";
 export function checkDhis2Validations(validations: D2ValidationResponse[]): ConsistencyError[] {
     const errors = _(
         validations.map(({ validationRuleViolations }) => {
-            validationRuleViolations.map(rulesViolation => {
-                return i18n.t(
-                    `Validation rule '${(rulesViolation as any).validationRule.name}' violated. Left side value: '${
-                        (rulesViolation as any).leftsideValue
-                    }', right side value: '${(rulesViolation as any).rightsideValue}'`
-                );
-            });
+            if (validationRuleViolations.length) {
+                return validationRuleViolations.map(rulesViolation => {
+                    return i18n.t(
+                        `Validation rule '${(rulesViolation as any).validationRule.name}' violated. Left side value: '${
+                            (rulesViolation as any).leftsideValue
+                        }', right side value: '${(rulesViolation as any).rightsideValue}'`
+                    );
+                });
+            }
         })
     )
+        .omitBy(_.isUndefined)
         .groupBy(error => error)
         .mapValues(values => values.length)
         .value();
