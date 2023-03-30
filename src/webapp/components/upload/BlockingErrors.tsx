f96728ba8a102be0420d057e6d659eebc47b7cb1
davidbgomes <davidbgomes92@gmail.com>
2023-03-30 23:05:56 +0100

    Add modal with full line content if too big

--- src/webapp/components/upload/BlockingErrors.tsx
+++ src/webapp/components/upload/BlockingErrors.tsx
@@ -1,13 +1,62 @@
-import React from "react";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+    Button,
+    DialogContent,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+} from "@material-ui/core";
 import styled from "styled-components";
 import i18n from "@eyeseetea/d2-ui-components/locales";
 import { ConsistencyError } from "../../../domain/entities/data-entry/ImportSummary";
+import { ConfirmationDialog } from "@eyeseetea/d2-ui-components";
 
 interface BlockingErrorsProps {
     rows: ConsistencyError[];
 }
+
+const RowLines = ({
+    row,
+    setIsOpen,
+    setCurrentRowLines,
+}: {
+    row: ConsistencyError;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    setCurrentRowLines: React.Dispatch<React.SetStateAction<number[]>>;
+}): JSX.Element => {
+    if (row.lines) {
+        if (row.lines.length > 5) {
+            return (
+                <Typography>
+                    {row.lines?.slice(0, 5).join(", ")}
+                    {", "}
+                    <Button
+                        key={row.error}
+                        onClick={() => {
+                            setIsOpen(true);
+                            setCurrentRowLines(row.lines || []);
+                        }}
+                        variant="text"
+                        style={{ minWidth: "17px", padding: "3px 0px" }}
+                    >
+                        ...
+                    </Button>
+                </Typography>
+            );
+        } else {
+            return <Typography>{row.lines?.join(", ")}</Typography>;
+        }
+    } else return <Typography>-</Typography>;
+};
+
 export const BlockingErrors: React.FC<BlockingErrorsProps> = ({ rows }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentRowLines, setCurrentRowLines] = useState<number[]>([]);
     return (
         <ContentWrapper>
             <Typography variant="h3">{i18n.t("Blocking Errors")}</Typography>
@@ -27,13 +76,31 @@ export const BlockingErrors: React.FC<BlockingErrorsProps> = ({ rows }) => {
                                 <TableRow key={row.error}>
                                     <TableCell align="left">{row.error}</TableCell>
                                     <TableCell>{row.count}</TableCell>
-                                    <TableCell>{row.lines?.join(", ") || "-"}</TableCell>
+                                    <TableCell>
+                                        <RowLines
+                                            row={row}
+                                            setIsOpen={setIsOpen}
+                                            setCurrentRowLines={setCurrentRowLines}
+                                        />
+                                    </TableCell>
                                 </TableRow>
                             );
                         })}
                     </TableBody>
                 </Table>
             </TableContainer>
+            <ConfirmationDialog
+                isOpen={isOpen}
+                title={i18n.t("Blocked Lines")}
+                onCancel={() => setIsOpen(false)}
+                cancelText={i18n.t("Done")}
+                fullWidth={true}
+                disableEnforceFocus
+            >
+                <DialogContent>
+                    <Typography>{currentRowLines.join(", ")}</Typography>
+                </DialogContent>
+            </ConfirmationDialog>
         </ContentWrapper>
     );
 };
