61337fa9a0b753e3b5b25feaf0685a6dcec8e3e1
Sneha N <9sneha.n@gmail.com>
2023-04-10 15:57:21 +0530

    fix: remove unwanted file

--- src/domain/repositories/GlassDashboardsRepository.ts
+++
@@ -1,6 +0,0 @@
-import { FutureData } from "../entities/Future";
-
-export interface GlassDashboardsRepository {
-    getReportMenuDashboard(): FutureData<string>;
-    getReviewSummaryDashboard(): FutureData<string>;
-}
