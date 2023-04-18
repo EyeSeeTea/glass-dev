9d143191386b89d61c1ff02ac27552f7d8f03c0d
Sneha N <9sneha.n@gmail.com>
2023-04-18 12:54:15 +0530

    feat: send notification to capture group on COMPLETE

--- src/domain/repositories/NotificationRepository.ts
+++ src/domain/repositories/NotificationRepository.ts
@@ -1,8 +1,10 @@
 import { Id } from "@eyeseetea/d2-api";
 import { FutureData } from "../entities/Future";
 import { Notification } from "../entities/Notifications";
+import { Ref } from "../entities/Ref";
 
 export interface NotificationRepository {
     getAll(): FutureData<Notification[]>;
     get(id: Id): FutureData<Notification>;
+    send(subject: string, message: string, userGroupIds: Ref[], organisationUnit: Ref): FutureData<void>;
 }
