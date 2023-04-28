9477a847c79f09fe9e3c4d8f30d3db3ae8a9b75b
Sneha N <9sneha.n@gmail.com>
2023-04-28 21:22:19 +0530

    fix: open data submissions period as string

--- src/data/repositories/GlassDataSubmissionDefaultRepository.ts
+++ src/data/repositories/GlassDataSubmissionDefaultRepository.ts
@@ -32,7 +32,7 @@ export class GlassDataSubmissionsDefaultRepository implements GlassDataSubmissio
         return this.dataStoreClient.getObjectsFilteredByProps<GlassDataSubmission>(
             DataStoreKeys.DATA_SUBMISSIONS,
             new Map<keyof GlassDataSubmission, unknown>([
-                ["period", new Date().getFullYear() - 1], //Open Data Submissions are for the previous year
+                ["period", `${new Date().getFullYear() - 1}`], //Open Data Submissions are for the previous year
                 ["orgUnit", orgUnit],
             ])
         );
