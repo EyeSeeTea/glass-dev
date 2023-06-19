c1097505dc4116d67f6ea2f328bde09414ad7e56
Sneha N <9sneha.n@gmail.com>
2023-06-19 22:27:26 +0530

    feat: custom validation for unique Patient ID and event date  within org unit

--- src/domain/usecases/data-entry/egasp/CustomValidationForEGASP.ts
+++ src/domain/usecases/data-entry/egasp/CustomValidationForEGASP.ts
@@ -6,6 +6,7 @@ import { EventResult } from "../../../entities/program-rules/EventEffectTypes";
 import { firstDayOfQuarter, lastDayOfQuarter } from "../../../utils/quarterlyPeriodHelper";
 
 const EGASP_DATAELEMENT_ID = "KaS2YBRN8eH";
+const PATIENT_DATAELEMENT_ID = "aocFHBxcQa0";
 export class CustomValidationForEGASP {
     constructor(private dhis2EventsDefaultRepository: Dhis2EventsDefaultRepository) {}
     public getValidatedEvents(events: Event[], orgUnit: string, period: string): FutureData<EventResult> {
@@ -13,11 +14,23 @@ export class CustomValidationForEGASP {
         const orgUnitErrors = this.checkCountry(events, orgUnit);
         //2. Quarterly period validation
         const periodErrors = this.checkPeriod(events, period);
-        //3. Duplicate EGASP ID validation
-        return this.checkUniqueEgaspId(events, orgUnit).flatMap(duplicateEGASPIdErrors => {
+
+        //Fetch all existing EGASP events for the given org unit
+        return this.dhis2EventsDefaultRepository.getEGASPEventsByOrgUnit(orgUnit).flatMap(existingEvents => {
+            //3. Duplicate EGASP ID within org unit validation
+            const duplicateEGASPIdErrors = this.checkUniqueEgaspId(events, existingEvents);
+
+            //4. Duplicate Patient ID and Event date combo within org unit validation
+            const duplicatePatientIdErrors = this.checkUniquePatientIdAndDate(events, existingEvents);
+
             const results: EventResult = {
                 events: events,
-                blockingErrors: [...orgUnitErrors, ...periodErrors, ...duplicateEGASPIdErrors],
+                blockingErrors: [
+                    ...orgUnitErrors,
+                    ...periodErrors,
+                    ...duplicateEGASPIdErrors,
+                    ...duplicatePatientIdErrors,
+                ],
                 nonBlockingErrors: [],
             };
             return Future.success(results);
@@ -75,46 +88,93 @@ export class CustomValidationForEGASP {
         }));
     }
 
-    private checkUniqueEgaspId(events: Event[], orgUnit: string): FutureData<ConsistencyError[]> {
-        //Fetch all existing events for the given org unit with non null egasp ids
-        return this.dhis2EventsDefaultRepository.getEGASPEventsByOrgUnit(orgUnit).flatMap(existingEvents => {
-            //1. Egasp ids of events in file.
-            const fileEgaspIDs = events.map(event => {
-                const egaspDataElement = event.dataValues.find(dv => dv.dataElement === EGASP_DATAELEMENT_ID);
-                if (egaspDataElement) return { eventId: event.event, egaspId: egaspDataElement.value };
-                else return null;
-            });
+    private checkUniqueEgaspId(fileEvents: Event[], existingEvents: Event[]): ConsistencyError[] {
+        //1. Egasp ids of events in file.
+        const fileEgaspIDs = fileEvents.map(event => {
+            const egaspDataElement = event.dataValues.find(dv => dv.dataElement === EGASP_DATAELEMENT_ID);
+            if (egaspDataElement) return { eventId: event.event, egaspId: egaspDataElement.value };
+            else return null;
+        });
 
-            //2. Egasp ids of existing events.
-            const existingEgaspIDs = existingEvents.map(event => {
-                const egaspDataElement = event.dataValues.find(dv => dv.dataElement === EGASP_DATAELEMENT_ID);
-                if (egaspDataElement) return { eventId: event.event, egaspId: egaspDataElement.value };
-                else return null;
-            });
+        //2. Egasp ids of existing events.
+        const existingEgaspIDs = existingEvents.map(event => {
+            const egaspDataElement = event.dataValues.find(dv => dv.dataElement === EGASP_DATAELEMENT_ID);
+            if (egaspDataElement) return { eventId: event.event, egaspId: egaspDataElement.value };
+            else return null;
+        });
 
-            const egaspIDs = _([...fileEgaspIDs, ...existingEgaspIDs])
-                .compact()
-                .value();
+        const egaspIDs = _([...fileEgaspIDs, ...existingEgaspIDs])
+            .compact()
+            .value();
 
-            const errors = _(egaspIDs)
-                .groupBy("egaspId")
-                .map(duplicateEgaspIdGroup => {
-                    if (duplicateEgaspIdGroup.length > 1) {
+        const errors = _(egaspIDs)
+            .groupBy("egaspId")
+            .map(duplicateEgaspIdGroup => {
+                if (duplicateEgaspIdGroup.length > 1) {
+                    return {
+                        error: i18n.t(`This EGASP ID already exists : ${duplicateEgaspIdGroup[0]?.egaspId}`),
+                        lines: _(duplicateEgaspIdGroup.map(event => parseInt(event.eventId)))
+                            .compact()
+                            .value(),
+                        count: duplicateEgaspIdGroup.length,
+                    };
+                }
+            })
+            .compact()
+            .value();
+
+        return errors;
+    }
+
+    private checkUniquePatientIdAndDate(fileEvents: Event[], existingEvents: Event[]): ConsistencyError[] {
+        //1. Patient ids of events in file.
+        const filePatientIDs = fileEvents.map(event => {
+            const patientDataElement = event.dataValues.find(dv => dv.dataElement === PATIENT_DATAELEMENT_ID);
+            if (patientDataElement)
+                return {
+                    eventId: event.event,
+                    patientIdAndDate: `${patientDataElement.value},${new Date(event.eventDate).toISOString()}`,
+                };
+            else return null;
+        });
+
+        //2. Egasp ids of existing events.
+        const existingPatientsIDs = existingEvents.map(event => {
+            const patientDataElement = event.dataValues.find(dv => dv.dataElement === PATIENT_DATAELEMENT_ID);
+            if (patientDataElement)
+                return {
+                    eventId: event.event,
+                    patientIdAndDate: `${patientDataElement.value},${new Date(event.eventDate).toISOString()}`,
+                };
+            else return null;
+        });
+
+        const patientIDs = _([...filePatientIDs, ...existingPatientsIDs])
+            .compact()
+            .value();
+
+        const errors = _(patientIDs)
+            .groupBy("patientIdAndDate")
+            .map(duplicatePatientIdGroup => {
+                if (duplicatePatientIdGroup.length > 1) {
+                    if (duplicatePatientIdGroup[0]) {
+                        const [patientId, eventDate] = duplicatePatientIdGroup[0]?.patientIdAndDate.split(",");
                         return {
                             error: i18n.t(
-                                `EGASP id is not unique, Duplicate EGASP id : ${duplicateEgaspIdGroup[0]?.egaspId}`
+                                `This date is already associated to the same Patient-ID. Please check if the current information has already been entered. If not, please check whether the Patient-ID or this date are correct.
+                            Patient Id: ${patientId}, Event Date: ${eventDate}`
                             ),
-                            lines: _(duplicateEgaspIdGroup.map(event => parseInt(event.eventId)))
+                            lines: _(duplicatePatientIdGroup.map(event => parseInt(event.eventId)))
                                 .compact()
                                 .value(),
-                            count: duplicateEgaspIdGroup.length,
+                            count: duplicatePatientIdGroup.length,
                         };
                     }
-                })
-                .compact()
-                .value();
+                }
+            })
+            .compact()
+            .value();
 
-            return Future.success(errors);
-        });
+        return errors;
     }
 }
