42bd984ac43c3a4a5b9eaa126fa11f5128cb4949
Sneha N <9sneha.n@gmail.com>
2023-07-25 13:32:51 +0530

    fix: show options for selection question type

--- src/domain/repositories/CaptureFormRepository.ts
+++ src/domain/repositories/CaptureFormRepository.ts
@@ -2,7 +2,5 @@ import { FutureData } from "../entities/Future";
 import { Question, Questionnaire } from "../entities/Questionnaire";
 
 export interface CaptureFormRepository {
-    get(): FutureData<Question[]>;
-
     getForm(): FutureData<Questionnaire>;
 }
