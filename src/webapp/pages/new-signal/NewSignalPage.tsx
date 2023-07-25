8d121759929879513c2055d0790e812ad60284d9
Sneha N <9sneha.n@gmail.com>
2023-07-25 17:02:37 +0530

    feat: Some UI enhancements.

--- src/webapp/pages/new-signal/NewSignalPage.tsx
+++ src/webapp/pages/new-signal/NewSignalPage.tsx
@@ -13,25 +13,36 @@ import { useStyles } from "../../components/questionnaire/QuestionnaireForm";
 import { useAppContext } from "../../contexts/app-context";
 import { Questionnaire } from "../../../domain/entities/Questionnaire";
 import { PageHeader } from "../../components/page-header/PageHeader";
+import { useSnackbar } from "@eyeseetea/d2-ui-components";
 
 export const NewSignalPage: React.FC = React.memo(() => {
     const { currentModuleAccess } = useCurrentModuleContext();
     const { compositionRoot } = useAppContext();
     const [questionnaire, setQuestionnaire] = useState<Questionnaire>();
-    // const [questions, setQuestions] = useState<Question[]>();
+    const [formVisibility, setFormVisibility] = useState<boolean>(false);
     const classes = useStyles();
+    const snackbar = useSnackbar();
 
     useEffect(() => {
         return compositionRoot.captureForm.getForm().run(
-            qs => {
-                setQuestionnaire(qs);
+            questionnaireForm => {
+                setQuestionnaire(questionnaireForm);
             },
-            err => console.debug(err)
+            err => snackbar.error(err)
         );
-    }, [compositionRoot]);
+    }, [compositionRoot, snackbar]);
 
     const setQuestion = () => {
         console.debug("Set Question called");
+    };
+
+    const showForm = () => {
+        setFormVisibility(true);
+    };
+
+    const hideForm = () => {
+        snackbar.info("Submission Success!");
+        setFormVisibility(false);
     };
 
     return (
@@ -47,38 +58,53 @@ export const NewSignalPage: React.FC = React.memo(() => {
                     </Button>
                 </StyledBreadCrumbs>
             </PreContent>
-            <div>
-                <PageHeader title={questionnaire?.name || ""} />
+            {!formVisibility && (
+                <CenteredDiv>
+                    <Button variant="contained" color="primary" onClick={showForm}>
+                        {i18n.t("Draft New Signal")}
+                    </Button>
+                </CenteredDiv>
+            )}
+            {formVisibility && (
+                <div>
+                    <PageHeader title={questionnaire?.name || ""} />
 
-                {questionnaire?.sections.map(section => {
-                    if (!section.isVisible) return null;
+                    {questionnaire?.sections.map(section => {
+                        if (!section.isVisible) return null;
 
-                    return (
-                        <div key={section.title} className={classes.wrapper}>
-                            <DataTable>
-                                <TableHead>
-                                    <DataTableRow>
-                                        <DataTableColumnHeader colSpan="2">
-                                            <span className={classes.header}>{section.title}</span>
-                                        </DataTableColumnHeader>
-                                    </DataTableRow>
-                                </TableHead>
+                        return (
+                            <div key={section.title} className={classes.wrapper}>
+                                <DataTable>
+                                    <TableHead>
+                                        <DataTableRow>
+                                            <DataTableColumnHeader colSpan="2">
+                                                <span className={classes.header}>{section.title}</span>
+                                            </DataTableColumnHeader>
+                                        </DataTableRow>
+                                    </TableHead>
 
-                                <TableBody>
-                                    {section.questions.map(question => (
-                                        <QuestionRow
-                                            key={question.id}
-                                            disabled={false}
-                                            question={question}
-                                            setQuestion={setQuestion}
-                                        />
-                                    ))}
-                                </TableBody>
-                            </DataTable>
-                        </div>
-                    );
-                })}
-            </div>
+                                    <TableBody>
+                                        {section.questions.map(question => (
+                                            <QuestionRow
+                                                key={question.id}
+                                                disabled={false}
+                                                question={question}
+                                                setQuestion={setQuestion}
+                                            />
+                                        ))}
+                                    </TableBody>
+                                </DataTable>
+                            </div>
+                        );
+                    })}
+
+                    <PageFooter>
+                        <Button variant="contained" color="primary" onClick={hideForm}>
+                            {i18n.t("Submit")}
+                        </Button>
+                    </PageFooter>
+                </div>
+            )}
         </ContentWrapper>
     );
 });
@@ -128,4 +154,15 @@ const StyledBreadCrumbs = styled(Breadcrumbs)`
     svg {
         color: ${palette.text.secondary};
     }
+`;
+const CenteredDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const PageFooter = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
 `;
