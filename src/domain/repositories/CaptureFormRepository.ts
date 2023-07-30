34b7ebb1a72fac91101f7a3aa40fc3d251072644
Sneha N <9sneha.n@gmail.com>
2023-07-31 01:18:07 +0530

    chore: update translations

--- src/domain/repositories/CaptureFormRepository.ts
+++ src/domain/repositories/CaptureFormRepository.ts
@@ -1,5 +1,5 @@
 import { FutureData } from "../entities/Future";
-import { Question, Questionnaire } from "../entities/Questionnaire";
+import { Questionnaire } from "../entities/Questionnaire";
 
 export interface CaptureFormRepository {
     getForm(): FutureData<Questionnaire>;
