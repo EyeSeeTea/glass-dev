279bbc127e723b491df393462ebcb41ebd8d4ce2
davidbgomes <davidbgomes92@gmail.com>
2023-08-17 13:34:09 +0100

    preheat cache on insert, improve performance

--- src/data/repositories/DataValuesDefaultRepository.ts
+++ src/data/repositories/DataValuesDefaultRepository.ts
@@ -23,6 +23,7 @@ export class DataValuesDefaultRepository implements DataValuesRepository {
                 {
                     importStrategy: action,
                     dryRun: dryRun,
+                    preheatCache: true,
                 },
                 { dataValues }
             )
