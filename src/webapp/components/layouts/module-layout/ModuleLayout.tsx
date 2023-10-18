e946183f6ad2dd8e9464c879bb2bc0ca5ef1834e
9sneha-n <9sneha.n@gmail.com>
2023-10-18 12:37:29 +0530

    fix: better naming and some refactoring

--- src/webapp/components/layouts/module-layout/ModuleLayout.tsx
+++ src/webapp/components/layouts/module-layout/ModuleLayout.tsx
@@ -2,17 +2,15 @@ import React from "react";
 
 import { MainLayout } from "../main-layout/MainLayout";
 import { useGlassModule } from "../../../hooks/useGlassModule";
-import { useAppContext } from "../../../contexts/app-context";
 import { ContentLoader } from "../../content-loader/ContentLoader";
 import { useHistory } from "react-router-dom";
 import { useCurrentModuleContext } from "../../../contexts/current-module-context";
 
 export const ModuleLayout: React.FC = ({ children }) => {
-    const { compositionRoot } = useAppContext();
     const history = useHistory();
     const { resetCurrentModuleAccess } = useCurrentModuleContext();
 
-    const module = useGlassModule(compositionRoot);
+    const module = useGlassModule();
 
     const onError = () => {
         resetCurrentModuleAccess();
