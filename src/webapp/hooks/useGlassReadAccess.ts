9eced806d76475d76a19f71c0cf3b9d8f467c94b
davidbgomes <davidbgomes92@gmail.com>
2023-10-24 16:15:42 +0100

    feat: check modules enrollment in readAccess hook

--- src/webapp/hooks/useGlassReadAccess.ts
+++ src/webapp/hooks/useGlassReadAccess.ts
@@ -3,20 +3,33 @@ import { GlassNews } from "../../domain/entities/GlassNews";
 import { useCurrentModuleContext } from "../contexts/current-module-context";
 import { useCurrentOrgUnitContext } from "../contexts/current-orgUnit-context";
 import { GlassState } from "./State";
+import { useSideBarModulesContext } from "../contexts/sidebar-modules-context";
 
 export type GlassNewsState = GlassState<GlassNews[]>;
 
 export function useGlassReadAccess() {
     const [hasReadAccess, setHasReadAccess] = React.useState<boolean>();
+
     const { currentModuleAccess } = useCurrentModuleContext();
     const { currentOrgUnitAccess } = useCurrentOrgUnitContext();
+    const { accessibleModules, isLoading } = useSideBarModulesContext();
 
     useEffect(() => {
-        //TO DO: add third condition for enrollment
         //Only set the read access, after the module and org unit access have been set
-        if (currentModuleAccess.moduleId !== "" && currentOrgUnitAccess.orgUnitId !== "")
-            setHasReadAccess(currentModuleAccess.readAccess && currentOrgUnitAccess.readAccess);
-    }, [currentModuleAccess, currentOrgUnitAccess]);
+        if (currentModuleAccess.moduleId !== "" && currentOrgUnitAccess.orgUnitId !== "" && !isLoading)
+            setHasReadAccess(
+                currentModuleAccess.readAccess &&
+                    currentOrgUnitAccess.readAccess &&
+                    accessibleModules.map(module => module.id).includes(currentModuleAccess.moduleId)
+            );
+    }, [
+        accessibleModules,
+        currentModuleAccess.moduleId,
+        currentModuleAccess.readAccess,
+        currentOrgUnitAccess.orgUnitId,
+        currentOrgUnitAccess.readAccess,
+        isLoading,
+    ]);
 
     return hasReadAccess;
 }
