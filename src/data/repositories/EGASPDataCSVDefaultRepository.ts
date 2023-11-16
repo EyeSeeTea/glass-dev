79a45ad0e504cd39bfd1520df2229103621d8655
Ana Garcia <ana@eyeseetea.com>
2023-11-16 15:28:58 +0100

    Add just rows to uploaded files

--- src/data/repositories/EGASPDataCSVDefaultRepository.ts
+++ src/data/repositories/EGASPDataCSVDefaultRepository.ts
@@ -3,10 +3,7 @@ import { EGASPDataRepository } from "../../domain/repositories/data-entry/EGASPD
 import { SpreadsheetXlsxDataSource } from "./SpreadsheetXlsxDefaultRepository";
 
 export class EGASPDataCSVDefaultRepository implements EGASPDataRepository {
-    validate(
-        file: File,
-        dataColumns: string[]
-    ): FutureData<{ isValid: boolean; records: number; specimens: string[] }> {
+    validate(file: File, dataColumns: string[]): FutureData<{ isValid: boolean; specimens: string[]; rows: number }> {
         return Future.fromPromise(new SpreadsheetXlsxDataSource().read(file)).map(spreadsheet => {
             const sheet = spreadsheet.sheets[0]; //First sheet has data for EGASP
             const headerRow = sheet?.rows[1]; //The second row has header deyails for EGASP template.
@@ -17,13 +14,13 @@ export class EGASPDataCSVDefaultRepository implements EGASPDataRepository {
 
                 return {
                     isValid: allEGASPColsPresent ? true : false,
-                    records: sheet.rows.length - 2, //two rows for header
+                    rows: sheet.rows.length - 2, //two rows for header
                     specimens: [],
                 };
             } else
                 return {
                     isValid: false,
-                    records: 0,
+                    rows: 0,
                     specimens: [],
                 };
         });
