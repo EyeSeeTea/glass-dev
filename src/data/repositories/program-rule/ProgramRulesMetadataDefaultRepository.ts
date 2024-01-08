525ef2d395cfb2e4a7d40561300206efe3cae089
9sneha-n <9sneha.n@gmail.com>
2024-01-09 00:27:38 +0530

    fix: remove unused code

--- src/data/repositories/program-rule/ProgramRulesMetadataDefaultRepository.ts
+++ src/data/repositories/program-rule/ProgramRulesMetadataDefaultRepository.ts
@@ -1,12 +1,10 @@
 import { D2Api } from "@eyeseetea/d2-api/2.34";
-import { Future, FutureData } from "../../../domain/entities/Future";
+import { FutureData } from "../../../domain/entities/Future";
 import { ProgramRulesMetadataRepository } from "../../../domain/repositories/program-rules/ProgramRulesMetadataRepository";
 import { apiToFuture } from "../../../utils/futures";
 import { Instance } from "../../entities/Instance";
 import { getD2APiFromInstance } from "../../../utils/d2-api";
 import { BulkLoadMetadata, metadataQuery } from "../../../domain/entities/program-rules/EventEffectTypes";
-import { Maybe } from "../../../types/utils";
-import { D2TrackerTrackedEntity } from "@eyeseetea/d2-api/api/trackerTrackedEntities";
 
 export const EGASP_PROGRAM_ID = "SOjanrinfuG";
 
@@ -30,40 +28,5 @@ export class ProgramRulesMetadataDefaultRepository implements ProgramRulesMetada
         ).map(baseMetadata => {
             return { ...baseMetadata, dataElementsById: _.keyBy(baseMetadata.dataElements, de => de.id) };
         });
-    }
-
-    async getTrackedEntityInstances(programId: string, orgUnitId: string): Promise<D2TrackerTrackedEntity[]> {
-        let page = 1;
-        const pageSize = 1000;
-        let total: Maybe<{ pages: number }>;
-        const allTeis: D2TrackerTrackedEntity[] = [];
-
-        do {
-            const res = await this.api.tracker.trackedEntities
-                .get({
-                    program: programId,
-
-                    fields: { enrollments: { events: true } },
-                    orgUnit: orgUnitId,
-                    ouMode: "SELECTED",
-                    enrollmentEnrolledBefore: new Date().toISOString(),
-                    totalPages: true,
-                    page: page,
-                    pageSize: pageSize,
-                })
-                .getData();
-
-            const teis = res.instances;
-            total = { pages: res.total ?? 0 };
-
-            allTeis.push(...teis);
-            page++;
-        } while (page <= total?.pages);
-
-        return allTeis;
-    }
-
-    getTEIs(programId: string, orgUnitId: string): FutureData<D2TrackerTrackedEntity[]> {
-        return Future.fromPromise(this.getTrackedEntityInstances(programId, orgUnitId));
     }
 }
