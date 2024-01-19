ed75305b51a6b758ff9ba202827885fd0c133ab0
Ana Garcia <ana@eyeseetea.com>
2024-01-19 13:12:18 +0100

    Get recalculations data from dataStore and allow to stop recalculations

---
+++ src/domain/usecases/data-entry/amc/DisableAMCRecalculationsUseCase.ts
@@ -0,0 +1,9 @@
+import { FutureData } from "../../../entities/Future";
+import { GlassATCRepository } from "../../../repositories/GlassATCRepository";
+
+export class DisableAMCRecalculationsUseCase {
+    constructor(private atcRepository: GlassATCRepository) {}
+    public execute(): FutureData<void> {
+        return this.atcRepository.disableRecalculations().toVoid();
+    }
+}
