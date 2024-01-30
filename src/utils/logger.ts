e4a654615bd8251a86d75dc3de6c5143408ea7bd
Ana Garcia <ana@eyeseetea.com>
2024-01-30 15:55:06 +0100

    Add @eyeseetea/d2-logger library

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,25 +3,25 @@ import { Instance } from "../data/entities/Instance";
 import { Id } from "../domain/entities/Base";
 
 // TODO: add here program and data elements
-// const LOGS_PROGRAM = "zARxYmOD18Z";
-// const MESSAGE_DATA_ELEMENT = "BjUzF5E4eR8";
-// const MESSAGE_TYPE_DATA_ELEMENT = "NpS5LoLuhgS";
+const LOGS_PROGRAM = "zARxYmOD18Z";
+const MESSAGE_DATA_ELEMENT = "BjUzF5E4eR8";
+const MESSAGE_TYPE_DATA_ELEMENT = "NpS5LoLuhgS";
 
 export let logger: Logger;
 
 export async function setupLogger(instance: Instance, orgUnitId?: Id): Promise<void> {
     const loggerInstance = new Logger();
-    // await loggerInstance.init({
-    //     type: "program",
-    //     baseUrl: instance.url,
-    //     auth: instance.auth,
-    //     programId: LOGS_PROGRAM,
-    //     organisationUnitId: orgUnitId,
-    //     loggerDataElements: {
-    //         messageId: MESSAGE_DATA_ELEMENT,
-    //         messageTypeId: MESSAGE_TYPE_DATA_ELEMENT,
-    //     },
-    // });
+    await loggerInstance.init({
+        type: "program",
+        baseUrl: instance.url,
+        auth: instance.auth,
+        programId: LOGS_PROGRAM,
+        organisationUnitId: orgUnitId,
+        loggerDataElements: {
+            messageId: MESSAGE_DATA_ELEMENT,
+            messageTypeId: MESSAGE_TYPE_DATA_ELEMENT,
+        },
+    });
     logger = loggerInstance;
 }
 
