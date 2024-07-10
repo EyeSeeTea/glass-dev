e44c63cb707baa480508a556bad2047c544c2590
9sneha-n <9sneha.n@gmail.com>
2024-07-11 01:10:10 +0530

    feat: handle specimen-pathogen-antibiotic mapping for consistency checks

--- src/domain/usecases/data-entry/utils/checkSpecimenPathogen.ts
+++ src/domain/usecases/data-entry/utils/checkSpecimenPathogen.ts
@@ -1,22 +1,34 @@
 import i18n from "../../../../locales";
 import { RISData } from "../../../entities/data-entry/amr-external/RISData";
 import { ConsistencyError } from "../../../entities/data-entry/ImportSummary";
+import { PATHOGEN_ANTIBIOTIC_MAP } from "../../../entities/GlassModule";
 
 export function checkSpecimenPathogen(
     risData: RISData[],
-    specimenPathogenValidations: Record<string, string[]>
+    specimenPathogenValidations: Record<string, PATHOGEN_ANTIBIOTIC_MAP[]>
 ): ConsistencyError[] {
     const errors = _(
         risData.map((item, index) => {
             const allowPathogens = specimenPathogenValidations[item.SPECIMEN];
 
-            if (!allowPathogens?.includes(item.PATHOGEN)) {
+            const pathogenAntibiotics = allowPathogens?.find(pathogen => pathogen[item.PATHOGEN]);
+
+            if (!pathogenAntibiotics) {
                 return {
                     error: i18n.t(
-                        `The specimen and pathogen combination you have provided is not allowed. Specimen ${item.SPECIMEN} Pathogen ${item.PATHOGEN}`
+                        `The specimen, pathogen, antibiotic combination you have provided is not allowed. Specimen ${item.SPECIMEN}, Pathogen ${item.PATHOGEN}, Antibiotic ${item.ANTIBIOTIC}`
                     ),
                     line: index + 1,
                 };
+            } else {
+                if (!pathogenAntibiotics?.[item.PATHOGEN]?.includes(item.ANTIBIOTIC)) {
+                    return {
+                        error: i18n.t(
+                            `The specimen, pathogen, antibiotic combination you have provided is not allowed. Specimen ${item.SPECIMEN}, Pathogen ${item.PATHOGEN}, Antibiotic ${item.ANTIBIOTIC}`
+                        ),
+                        line: index + 1,
+                    };
+                }
             }
         })
     )
