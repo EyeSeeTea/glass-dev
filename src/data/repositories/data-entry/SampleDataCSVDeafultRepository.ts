7d598249049cc1238d9820ae754232fc02f87649
Ana Garcia <ana@eyeseetea.com>
2024-10-29 12:22:38 +0100

    Add DeleteSampleFileUseCase and improve code

--- src/data/repositories/data-entry/SampleDataCSVDeafultRepository.ts
+++ src/data/repositories/data-entry/SampleDataCSVDeafultRepository.ts
@@ -1,6 +1,7 @@
 import { SampleData } from "../../../domain/entities/data-entry/amr-external/SampleData";
 import { Future, FutureData } from "../../../domain/entities/Future";
 import { SampleDataRepository } from "../../../domain/repositories/data-entry/SampleDataRepository";
+import { Row } from "../../../domain/repositories/SpreadsheetXlsxRepository";
 import { SpreadsheetXlsxDataSource } from "../SpreadsheetXlsxDefaultRepository";
 import { doesColumnExist, getNumberValue, getTextValue } from "../utils/CSVUtils";
 
@@ -8,22 +9,30 @@ export class SampleDataCSVDeafultRepository implements SampleDataRepository {
     get(file: File): FutureData<SampleData[]> {
         return Future.fromPromise(new SpreadsheetXlsxDataSource().read(file)).map(spreadsheet => {
             const sheet = spreadsheet.sheets[0]; //Only one sheet for AMR RIS
+            return this.mapSheetRowsToSampleData(sheet?.rows || []);
+        });
+    }
 
-            return (
-                sheet?.rows.map(row => {
-                    return {
-                        COUNTRY: getTextValue(row, "COUNTRY"),
-                        YEAR: getNumberValue(row, "YEAR"),
-                        SPECIMEN: getTextValue(row, "SPECIMEN"),
-                        GENDER: getTextValue(row, "GENDER"),
-                        ORIGIN: getTextValue(row, "ORIGIN"),
-                        AGEGROUP: getTextValue(row, "AGEGROUP"),
-                        NUMINFECTED: getNumberValue(row, "NUMINFECTED"),
-                        NUMSAMPLEDPATIENTS: getNumberValue(row, "NUMSAMPLEDPATIENTS"),
-                        BATCHIDDS: getTextValue(row, "BATCHID"),
-                    };
-                }) || []
-            );
+    getFromArayBuffer(arrayBuffer: ArrayBuffer): FutureData<SampleData[]> {
+        return Future.fromPromise(new SpreadsheetXlsxDataSource().readFromArrayBuffer(arrayBuffer)).map(spreadsheet => {
+            const sheet = spreadsheet.sheets[0]; //Only one sheet for AMR RIS
+            return this.mapSheetRowsToSampleData(sheet?.rows || []);
+        });
+    }
+
+    private mapSheetRowsToSampleData(rows: Row<string>[]): SampleData[] {
+        return rows.map(row => {
+            return {
+                COUNTRY: getTextValue(row, "COUNTRY"),
+                YEAR: getNumberValue(row, "YEAR"),
+                SPECIMEN: getTextValue(row, "SPECIMEN"),
+                GENDER: getTextValue(row, "GENDER"),
+                ORIGIN: getTextValue(row, "ORIGIN"),
+                AGEGROUP: getTextValue(row, "AGEGROUP"),
+                NUMINFECTED: getNumberValue(row, "NUMINFECTED"),
+                NUMSAMPLEDPATIENTS: getNumberValue(row, "NUMSAMPLEDPATIENTS"),
+                BATCHIDDS: getTextValue(row, "BATCHID"),
+            };
         });
     }
 
