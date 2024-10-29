600daaf5a62eeeb347884beff03d7e2f22805c4f
Ana Garcia <ana@eyeseetea.com>
2024-10-29 11:28:12 +0100

    Add DeleteRISIndividualFungalFileUseCase

---
+++ src/domain/usecases/data-entry/amr-individual-fungal/DeleteRISIndividualFungalFileUseCase.ts
@@ -0,0 +1,31 @@
+import { FutureData } from "../../../entities/Future";
+import { ImportSummary } from "../../../entities/data-entry/ImportSummary";
+import { GlassDocumentsRepository } from "../../../repositories/GlassDocumentsRepository";
+import { TrackerRepository } from "../../../repositories/TrackerRepository";
+import { MetadataRepository } from "../../../repositories/MetadataRepository";
+import { downloadIdsAndDeleteTrackedEntities } from "../utils/downloadIdsAndDeleteTrackedEntities";
+import { UseCase } from "../../../../CompositionRoot";
+import { AMR_GLASS_AMR_TET_PATIENT } from "./ImportRISIndividualFungalFile";
+
+// NOTICE: code adapted for node environment from ImportRISIndividualFungalFile.ts (only DELETE)
+export class DeleteRISIndividualFungalFileUseCase implements UseCase {
+    constructor(
+        private options: {
+            trackerRepository: TrackerRepository;
+            glassDocumentsRepository: GlassDocumentsRepository;
+            metadataRepository: MetadataRepository;
+        }
+    ) {}
+
+    public execute(orgUnitId: string, eventListId: string | undefined): FutureData<ImportSummary> {
+        return downloadIdsAndDeleteTrackedEntities(
+            eventListId,
+            orgUnitId,
+            "DELETE",
+            AMR_GLASS_AMR_TET_PATIENT,
+            this.options.glassDocumentsRepository,
+            this.options.trackerRepository,
+            this.options.metadataRepository
+        );
+    }
+}
