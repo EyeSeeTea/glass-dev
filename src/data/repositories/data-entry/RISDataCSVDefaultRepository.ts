ed7e192ace5c33db1cd0b5bc98679949c41801e8
Ana Garcia <ana@eyeseetea.com>
2024-10-29 10:37:56 +0100

    Implement DeleteRISFileUseCase (primary file AMR)

--- src/data/repositories/data-entry/RISDataCSVDefaultRepository.ts
+++ src/data/repositories/data-entry/RISDataCSVDefaultRepository.ts
@@ -34,6 +34,35 @@ export class RISDataCSVDefaultRepository implements RISDataRepository {
         });
     }
 
+    getFromArayBuffer(arrayBuffer: ArrayBuffer): FutureData<RISData[]> {
+        return Future.fromPromise(new SpreadsheetXlsxDataSource().readFromArrayBuffer(arrayBuffer)).map(spreadsheet => {
+            const sheet = spreadsheet.sheets[0]; //Only one sheet for AMR RIS
+
+            return (
+                sheet?.rows.map(row => {
+                    return {
+                        COUNTRY: getTextValue(row, "COUNTRY"),
+                        YEAR: getNumberValue(row, "YEAR"),
+                        SPECIMEN: getTextValue(row, "SPECIMEN"),
+                        PATHOGEN: getTextValue(row, "PATHOGEN"),
+                        GENDER: getTextValue(row, "GENDER"),
+                        ORIGIN: getTextValue(row, "ORIGIN"),
+                        AGEGROUP: getTextValue(row, "AGEGROUP"),
+                        ANTIBIOTIC: getTextValue(row, "ANTIBIOTIC"),
+                        RESISTANT: getNumberValue(row, "RESISTANT"),
+                        INTERMEDIATE: getNumberValue(row, "INTERMEDIATE"),
+                        NONSUSCEPTIBLE: getNumberValue(row, "NONSUSCEPTIBLE"),
+                        SUSCEPTIBLE: getNumberValue(row, "SUSCEPTIBLE"),
+                        UNKNOWN_NO_AST: getNumberValue(row, "UNKNOWN_NO_AST"),
+                        UNKNOWN_NO_BREAKPOINTS: getNumberValue(row, "UNKNOWN_NO_BREAKPOINTS"),
+                        BATCHIDDS: getTextValue(row, "BATCHID"),
+                        ABCLASS: this.validateABCLASS(getTextValue(row, "ABCLASS")),
+                    };
+                }) || []
+            );
+        });
+    }
+
     validate(file: File): FutureData<{ isValid: boolean; specimens: string[]; rows: number }> {
         return Future.fromPromise(new SpreadsheetXlsxDataSource().read(file)).map(spreadsheet => {
             const sheet = spreadsheet.sheets[0]; //Only one sheet for AMR RIS
