9b1e92f0de187d4ad6b9fd2a2e21d0dce5619388
9sneha-n <9sneha.n@gmail.com>
2025-01-13 00:52:00 +0530

    show loader for data submission fetch

--- src/webapp/hooks/useCurrentDataSubmissionId.ts
+++ src/webapp/hooks/useCurrentDataSubmissionId.ts
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../contexts/app-context";
+import { useSnackbar } from "@eyeseetea/d2-ui-components";
 
 export function useCurrentDataSubmissionId(moduleId: string, moduleName: string, orgUnit: string, period: string) {
     const { currentUser, compositionRoot } = useAppContext();
     const [currentDataSubmissionId, setCurrentDataSubmissionId] = useState<string>("");
+    const snackbar = useSnackbar();
 
     useEffect(() => {
-        async function getDataSubmission() {
-            const isQuarterlyModule = currentUser.quarterlyPeriodModules.find(m => m.id === moduleId) ? true : false;
-            const currentDataSubmission = await compositionRoot.glassDataSubmission
-                .getSpecificDataSubmission(moduleId, moduleName, orgUnit, period, isQuarterlyModule)
-                .toPromise();
-            setCurrentDataSubmissionId(currentDataSubmission.id);
-        }
-        getDataSubmission();
+        const isQuarterlyModule = currentUser.quarterlyPeriodModules.find(m => m.id === moduleId) ? true : false;
+        compositionRoot.glassDataSubmission
+            .getSpecificDataSubmission(moduleId, moduleName, orgUnit, period, isQuarterlyModule)
+            .run(
+                currentDataSubmission => {
+                    setCurrentDataSubmissionId(currentDataSubmission.id);
+                },
+                error => {
+                    snackbar.error("Error fetching data submission : " + error);
+                }
+            );
     }, [
         compositionRoot.glassDataSubmission,
         moduleId,
@@ -21,6 +26,7 @@ export function useCurrentDataSubmissionId(moduleId: string, moduleName: string,
         orgUnit,
         period,
         currentUser.quarterlyPeriodModules,
+        snackbar,
     ]);
 
     return currentDataSubmissionId;
