8eca0f06def1ebacbe781cbec6cd8be515a1985b
Ana Garcia <ana@eyeseetea.com>
2025-04-22 12:47:23 +0200

    Fix problem when checking rule and add initial required in form state

--- src/webapp/components/current-data-submission/amc-questionnaires/useAMCQuestionnaireForm.ts
+++ src/webapp/components/current-data-submission/amc-questionnaires/useAMCQuestionnaireForm.ts
@@ -98,7 +98,7 @@ export function useAMCQuestionnaireForm(params: {
     }, [amcQuestions, compositionRoot.amcQuestionnaires, formState.kind]);
 
     useEffect(() => {
-        if (amcQuestions) {
+        if (amcQuestions && options) {
             if (id) {
                 switch (formType) {
                     case "general-questionnaire":
