095fe0e911d597b70e53c60cc9ca24a32d8f95fe
Matias Arriola <matias@eyeseetea.com>
2025-05-08 13:44:42 -0300

    Add AMClass validations

--- src/domain/entities/amc-questionnaires/ValidationError.ts
+++ src/domain/entities/amc-questionnaires/ValidationError.ts
@@ -2,6 +2,7 @@ import { Maybe } from "../../../utils/ts-utils";
 
 export enum ValidationErrorKey {
     FIELD_IS_REQUIRED = "field_is_required",
+    CANNOT_CREATE_AM_CLASS_QUESTIONNAIRE_NOT_CHECKED = "cannot_create_am_class_questionnaire_not_checked",
     CANNOT_CREATE_DUPLICATE_AM_CLASS_QUESTIONNAIRE = "cannot_create_duplicate_am_class_questionnaire",
 }
 
