5781845e445150cb95cce6419cebd676a3ae46c1
dodwho <odohertyd@who.int>
2026-05-08 16:02:22 +0200

    fix lint issues

--- src/scripts/daire-personal-scripts/node-web-apis.ts
+++ src/scripts/daire-personal-scripts/node-web-apis.ts
@@ -1,10 +1,9 @@
 // src/polyfills/node-web-apis.ts
 
-import { TextEncoder, TextDecoder } from "util";
+import { Blob as PBlob, File as PFile, FormData as PFormData } from "formdata-node";
 
 const g = globalThis as any;
 // 2) Blob / File / FormData (formdata-node polyfill)
-import { Blob as PBlob, File as PFile, FormData as PFormData } from "formdata-node";
 g.Blob ??= PBlob;
 g.File ??= PFile;
 g.FormData ??= PFormData;
